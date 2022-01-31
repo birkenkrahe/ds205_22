050c7e94369b2f80ec5a17fb9620ffa9ae457013
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2022-01-31 07:12:12 -0600

    updates

---
+++ assignments/org-mode/solutions/first.R
@@ -0,0 +1,12 @@
+## My first Org-mode file
+
+##   This Org-mode file prints the structure of the data frame ~mtcars~
+##   and creates a source code file ~first.R~ that can be executed on
+##   the command line.
+
+##   #+name: str_mtcars
+
+## [[file:MarcusBirkenkrahe.org::str_mtcars][str_mtcars]]
+## print structure of mtcars data frame
+str(mtcars)
+## str_mtcars ends here
